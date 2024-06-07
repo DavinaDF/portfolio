b163f9ac385d5cb02e2d778266739b307a6ea409
Davina Crahet <davina.crahet@gmail.com>
2024-06-07 10:42:27 +0200

    Contact bas de page

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -15,7 +15,8 @@ const ProjectPreview = () => {
                                 <img src={skillTag} alt="Domaine de compétence du projet" />
                                 <h5>{project.category}</h5>
                             </div>
-                            <a href={project.link}>Lien Github</a>
+
+                            <a href={project.link} target="_blank">Lien Github</a>
                             
                         </div>
                         <div className="text-container">
