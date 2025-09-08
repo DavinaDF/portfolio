70cc91e7d965b2186a6b93a5ed496ba512382cc6
Davina Crahet <davina.crahet@gmail.com>
2025-09-08 18:17:48 +0200

    css accordion mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import { div } from "motion/react-client";
 import infoData from "../../src/data/infos.json";
+import AccordionItem from "../components/AccordionItem";
 
 const About = () => {
   const aboutData = infoData.about;
@@ -11,6 +12,9 @@ const About = () => {
           <div className="about-card" key={index}>
             <h3 className="about-title">{aboutCard.title}</h3>
             <div className="about-content">{aboutCard.content}</div>
+            <AccordionItem title={aboutCard.title}>
+              {aboutCard.content}
+            </AccordionItem>
           </div>
         );
       })}
